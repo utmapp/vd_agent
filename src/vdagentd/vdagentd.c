e4bfd1b632b6c14e8411dbe3565115a78cd3d256
Uri Lublin <uril@redhat.com>
2020-10-07 19:34:57 +0300

    cleanup active_xfers when the client disconnects
    Signed-off-by: Uri Lublin <uril@redhat.com>
    Acked-by: Frediano Ziglio <fziglio@redhat.com>

--- src/vdagentd/vdagentd.c
+++ src/vdagentd/vdagentd.c
@@ -168,6 +168,7 @@ static void send_capabilities(VirtioPort *vport,
 
 static void do_client_disconnect(void)
 {
+    g_hash_table_remove_all(active_xfers);
     if (client_connected) {
         udscs_server_write_all(server, VDAGENTD_CLIENT_DISCONNECTED, 0, 0,
                                NULL, 0);
